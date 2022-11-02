a54059812e26e772a9c4cb7e8b057e698763129c
ObiedMohamed <abdelrahman.mohamed@almoasherbiz.com>
2022-11-02 13:31:32 +0200

    [ADD]

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'List View Freeze Header ',
+    'version': '15.0.1',
+    'summary': 'Allow to Freeze Header in List View',
+    'description': 'Allow to Freeze Header in List View',
+    'category': 'web',
+    'author': 'Abdulrhman Mohammed',
+    'company': 'Abdulrhman Mohammed',
+    'maintainer': 'Abdurlhman Mohammed',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'assets': {
+        'web.assets_backend': [
+            "list_view_freeze_header/static/src/scss/freeze_header.scss"
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
